9b39ade2e7824e45f6d88cf80a725f9cb10a5b1e
Kaiser137 <165351269+Kaiser137@users.noreply.github.com>
2024-08-13 21:03:38 -0300

    Add files via upload

---
+++ atv1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    printf("Atividade 1\n");
+    printf("\t  *  \n");
+    printf("\t *** \n");
+    printf("\t*****\n");
+    printf("\t /|\\\n\n");
+
+    printf("Atividade 2\n");
+    printf("Total\t=\t100%%\n");
+    printf("IVA\t=\t17%%\n");
+    printf("IRS\t=\t15%%\n");
+    printf("----------------------\n");
+    printf("LIQ\t=\t68%%\n\n");
+
+    printf("Atividade 3\n");
+    printf("Bem-vindos ao mundo da /programacao\\ em \"C\"\n\n");
+
+    printf("Atividade 4\n");
+    printf("A funcao \"printf\" da lingugem C, serve para escrever coisas para um usuário ler\n\n");
+
+    printf("Atividade 5\n");
+    printf("O erro presente na seguinte linha: \n");
+    printf("\"printf(\"Oi mundo\")\"\n");
+    printf("Se dá pela falta do ; no final da linha, sendo o correto:\n");
+    printf("\"printf(\"Oi mundo\");\"\n");
+}
