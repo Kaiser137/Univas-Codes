cc7e1d0aaa029c8831044b61fc5f73a2b2011716
Kaiser137 <165351269+Kaiser137@users.noreply.github.com>
2024-08-13 21:12:14 -0300

    Delete carac.c

--- carac.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    printf("\\n -> Nova Linha\n");
-    printf("\\t -> Tabulacao\n");
-    printf("\\\\ -> Escrever \\\n");
-    printf("\\\"\\\" -> Escapar aspas");
-}
