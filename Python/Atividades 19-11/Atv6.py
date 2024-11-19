0daa2156a09feac192c02342da35b5a8922bde33
Cezar Vitor <165351269+Kaiser137@users.noreply.github.com>
2024-11-19 17:43:40 -0300

    Create Atv6.py

---
+++ Python/Atividades 19-11/Atv6.py
@@ -0,0 +1,15 @@
+produtos = {
+    "Leite": 4.50,
+    "Queijo": 25.00,
+    "Iogurte": 3.00,
+    "Manteiga": 8.50,
+    "Requeijão": 6.00
+}
+
+produtos.update({
+    "Pão": 5.00,
+    "Croissant": 3.50,
+    "Bolo": 20.00
+})
+
+print(produtos)
