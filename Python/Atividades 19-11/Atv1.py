8edd5ebf9b6fd27f20e796e566628a8f1b6d4ba5
Cezar Vitor <165351269+Kaiser137@users.noreply.github.com>
2024-11-19 17:42:04 -0300

    Update Atv1.py

--- Python/Atividades 19-11/Atv1.py
+++ Python/Atividades 19-11/Atv1.py
@@ -1,1 +1,11 @@
+estados = {
+    "São Paulo": "SP",
+    "Rio de Janeiro": "RJ",
+    "Minas Gerais": "MG",
+    "Bahia": "BA"
+}
 
+if "Paraná" in estados:
+    print("O estado Paraná existe no dicionário.")
+else:
+    print("O estado Paraná não existe no dicionário.")
