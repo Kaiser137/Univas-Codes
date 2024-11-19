c90296a81a2508fa06ecb83d6fc53b7576b58987
Cezar Vitor <165351269+Kaiser137@users.noreply.github.com>
2024-11-19 17:43:01 -0300

    Create Atv4.py

---
+++ Python/Atividades 19-11/Atv4.py
@@ -0,0 +1,9 @@
+produtos = {
+    "Camiseta": 45.0,
+    "Calça": 89.90,
+    "Tênis": 120.0,
+    "Jaqueta": 250.0
+}
+
+maior_valor = max(produtos.values())
+print("O maior valor dos produtos é:", maior_valor)
