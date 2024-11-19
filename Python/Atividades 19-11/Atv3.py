8e35f9fb3375c65e53584c66caa71d98bcd2e129
Cezar Vitor <165351269+Kaiser137@users.noreply.github.com>
2024-11-19 17:42:43 -0300

    Create Atv3.py

---
+++ Python/Atividades 19-11/Atv3.py
@@ -0,0 +1,11 @@
+produtos = {
+    "Camiseta": 45.0,
+    "Calça": 89.90,
+    "Tênis": 120.0,
+    "Jaqueta": 250.0
+}
+
+if "Tênis" in produtos:
+    print("O produto Tênis existe no dicionário.")
+else:
+    print("O produto Tênis não existe no dicionário.")
